d15f011b3046a9848c9552d80e5db1c54ee3c337
samuel Adeyinka <tboysammy@gmail.com>
2024-09-27 23:15:07 +0100

    immutable

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,14 +1,5 @@
-import { Map } from 'immutable';
+import { List, Map } from 'immutable';
 
-export const map = Map({
-  1: 'Liam',
-  2: 'Noah',
-  3: 'Elijah',
-  4: 'Oliver',
-  5: 'Jacob',
-  6: 'Lucas',
-});
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-export const map2 = map.withMutations((values) => {
-  values.set(2, 'Benjamin').set(4, 'Oliver');
-});
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
