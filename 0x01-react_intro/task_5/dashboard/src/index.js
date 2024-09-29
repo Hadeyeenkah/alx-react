d36068023b156043c50dc1e3f05875f3c6fa54a3
samuel Adeyinka <tboysammy@gmail.com>
2024-09-29 14:24:09 +0100

    react

---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Make sure you have this App component defined
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
